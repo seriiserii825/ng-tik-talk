c4ef0349931169115b96344edceae777fec84681
serii burduja <seriiburduja@gmail.com>
2026-07-01 16:55:11 +0300

    feat: created sidebar

--- src/app/layout/default-layout/default-layout.component.ts
+++ src/app/layout/default-layout/default-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-default-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './default-layout.component.html',
   styleUrl: './default-layout.component.css',
 })
