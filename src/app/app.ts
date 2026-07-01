a62638ae97cb72e8ad413743f857a0283c3ef3af
serii burduja <seriiburduja@gmail.com>
2026-07-01 11:57:58 +0300

    feat: service work

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCard } from './shared/profile-card/profile-card';
+import { ProfileService } from './services/profile.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCard],
+  imports: [RouterOutlet, ProfileCard, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('ng-wishlist');
+  profileService = inject(ProfileService);
+  profiles: any = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.profiles = data;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
 }
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { catchError, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +9,11 @@ export class ProfileService {
   http = inject(HttpClient);
   baseApiUrl = 'https://icherniakov.ru/yt-course';
   getTestAccounts() {
-    this.http.get(`${this.baseApiUrl}/test-accounts`);
+    return this.http.get(`${this.baseApiUrl}/account/test_accounts`).pipe(
+      catchError((err) => {
+        console.error(err);
+        return of([]);
+      }),
+    );
   }
 }
