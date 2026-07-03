bee55050b8817265373f2425f13759179ec1265a
seriiserii825 <seriiburduja@gmail.com>
2026-07-03 20:36:05 +0300

    feat: sidebar bottom link

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { SidebarMenuComponent } from '../sidebar-menu/sidebar-menu.component';
 import { SubscriberCardComponent } from '../subscriber-card-component/subscriber-card-component';
+import { SvgIconComponent } from '../svg-icon-component/svg-icon-component';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SidebarMenuComponent, SubscriberCardComponent],
+  imports: [SidebarMenuComponent, SubscriberCardComponent, RouterLink, SvgIconComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
