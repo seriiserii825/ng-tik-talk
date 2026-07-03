0711e3296e72617bbf4d2e11c96a9cf28445f49a
seriiserii825 <seriiburduja@gmail.com>
2026-07-03 20:24:12 +0300

    feat: subscriber component

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SidebarMenuComponent } from '../sidebar-menu/sidebar-menu.component';
+import { SubscriberCardComponent } from '../subscriber-card-component/subscriber-card-component';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SidebarMenuComponent],
+  imports: [SidebarMenuComponent, SubscriberCardComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
---
+++ src/app/components/subscriber-card-component/subscriber-card-component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscriber-card-component',
+  imports: [],
+  templateUrl: './subscriber-card-component.html',
+})
+export class SubscriberCardComponent {}
