afc59d3e9a25eedbd3db517e632832554e331ace
seriiserii825 <seriiburduja@gmail.com>
2026-07-03 21:43:35 +0300

    feat: loading

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,28 +1,21 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterLink } from '@angular/router';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url-pipe';
 import { ProfileService } from '../../services/profile.service';
 import { SidebarMenuComponent } from '../sidebar-menu/sidebar-menu.component';
 import { SubscriberCardComponent } from '../subscriber-card-component/subscriber-card-component';
 import { SvgIconComponent } from '../svg-icon-component/svg-icon-component';
-import { ImgUrlPipe } from '../../helpers/pipes/img-url-pipe';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [
-    SidebarMenuComponent,
-    SubscriberCardComponent,
-    RouterLink,
-    SvgIconComponent,
-    AsyncPipe,
-    ImgUrlPipe,
-  ],
+  imports: [SidebarMenuComponent, SubscriberCardComponent, RouterLink, SvgIconComponent, ImgUrlPipe],
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent implements OnInit {
   profileService = inject(ProfileService);
   me = this.profileService.me;
-  subscribers$ = this.profileService.getSubscribersShortList();
+  subscribers = toSignal(this.profileService.getSubscribersShortList());
 
   ngOnInit() {
     this.profileService.onInit();
