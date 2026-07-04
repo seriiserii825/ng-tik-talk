455423c9bab0fd79765a27d7f682c0ae7a8d082b
seriiserii825 <seriiburduja@gmail.com>
2026-07-04 08:50:15 +0300

    feat: about me

---
+++ src/app/components/about-me/about-me.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-me',
+  imports: [],
+  templateUrl: './about-me.html',
+})
+export class AboutMe {}
--- src/app/page/profile-page/profile-page.component.ts
+++ src/app/page/profile-page/profile-page.component.ts
@@ -6,10 +6,18 @@ import { ProfileHeaderComponent } from '../../components/profile-header-componen
 import { SvgIconComponent } from '../../components/svg-icon-component/svg-icon-component';
 import { ProfileService } from '../../services/profile.service';
 import { LoadingIcon } from '../../icons/loading-icon/loading-icon';
+import { AboutMe } from '../../components/about-me/about-me';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [ProfileHeaderComponent, RouterLink, SvgIconComponent, ProfileFollowers, LoadingIcon],
+  imports: [
+    ProfileHeaderComponent,
+    RouterLink,
+    SvgIconComponent,
+    ProfileFollowers,
+    LoadingIcon,
+    AboutMe,
+  ],
   templateUrl: './profile-page.component.html',
 })
 export class ProfilePageComponent implements OnInit {
