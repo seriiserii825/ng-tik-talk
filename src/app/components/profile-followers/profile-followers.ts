805d513545ac1ca8dd4f5cb703551af629dfddf5
seriiserii825 <seriiburduja@gmail.com>
2026-07-04 08:13:53 +0300

    feat: profile followers start

---
+++ src/app/components/profile-followers/profile-followers.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-followers',
+  imports: [],
+  templateUrl: './profile-followers.html',
+})
+export class ProfileFollowers {}
--- src/app/page/profile-page/profile-page.component.ts
+++ src/app/page/profile-page/profile-page.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ProfileHeaderComponent } from '../../components/profile-header-component/profile-header-component';
 import { RouterLink } from '@angular/router';
 import { SvgIconComponent } from '../../components/svg-icon-component/svg-icon-component';
+import { ProfileFollowers } from '../../components/profile-followers/profile-followers';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [ProfileHeaderComponent, RouterLink, SvgIconComponent],
+  imports: [ProfileHeaderComponent, RouterLink, SvgIconComponent, ProfileFollowers],
   templateUrl: './profile-page.component.html',
 })
 export class ProfilePageComponent {}
